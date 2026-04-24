5ce111cbfb57845f151534d2b4366274317fc6ec
rwhiting76 <rwhiting76@gmail.com>
2026-04-24 17:36:06 -0600

    Update branding: new favicon, icons, and web manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,8 +49,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="708def64-73a8-4736-81fd-520827bcb674"></script>
       </head>
       <body className="min-h-full flex flex-col bg-zinc-950 text-white">
